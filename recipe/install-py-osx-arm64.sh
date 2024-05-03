744927a7363a5f89462b1a0d18eca55da08ab21d
memento <claude.rc@gmail.com>
2024-05-03 14:31:46 -0500

    (dbg) Bad CPU debug osx-arm64

---
+++ recipe/install-py-osx-arm64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+echo "Test python --version"
+${PYTHON} --version
+
+echo "Test pip module"
+${PYTHON} -m pip --version
+
+echo "Install quil python package"
+${PYTHON} -m pip install quil \
+  --no-build-isolation \
+  --no-deps \
+  --only-binary :all: \
+  --no-index \
+  --find-links=${SRC_DIR}/wheels/ \
+  --prefix ${PREFIX} \
+  -vv
