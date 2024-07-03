fa44cc22b0c2b7e3b96200ba01ddcff50bc9a0d6
memento <claude.rc@gmail.com>
2024-07-03 18:35:42 -0500

    (dbg) trying locking the versions in Cargo.toml

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 set -ex
+# Try to lock the versions in Cargo.toml
+if [[ $(uname) == Linux ]]; then
+  sed -i -E 's/version\s*=\s*"/version = "=/' "${SRC_DIR}"/quil-py/Cargo.toml
+  sed -i -E 's/version\s*=\s*"/version = "=/' "${SRC_DIR}"/quil-cli/Cargo.toml
+elif [[ $(uname) == Darwin ]]; then
+  sed -i '' -E 's/version\s*=\s*"/version = "=/' "${SRC_DIR}"/quil-py/Cargo.toml
+  sed -i '' -E 's/version\s*=\s*"/version = "=/' "${SRC_DIR}"/quil-cli/Cargo.toml
+else
+  echo "Unsupported OS"
+  exit 1
+fi
 
 # Build quil-py and quil-cli wheels
 maturin build \
   --release \
   --manifest-path="${SRC_DIR}"/quil-py/Cargo.toml \
-  --offline \
+  --strip \
   --out "${SRC_DIR}"/wheels
 maturin build \
   --release \
   --manifest-path="${SRC_DIR}"/quil-cli/Cargo.toml \
-  --offline \
+  --strip \
   --out "${SRC_DIR}"/wheels
 
 # Update license file
