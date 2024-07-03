8b901355d9e5094c7307f246fe47f7cd1c4d9dad
memento <claude.rc@gmail.com>
2024-07-03 16:31:32 -0500

    (dbg) registered quil version on osx-64 is 0.11.0. bump to generate new package

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,12 @@ set -ex
 # Build quil-py and quil-cli wheels
 maturin build \
   --release \
-  --manifest-path=${SRC_DIR}/quil-py/Cargo.toml \
-  --out ${SRC_DIR}/wheels
+  --manifest-path="${SRC_DIR}"/quil-py/Cargo.toml \
+  --out "${SRC_DIR}"/wheels
 maturin build \
   --release \
-  --manifest-path=${SRC_DIR}/quil-cli/Cargo.toml \
-  --out ${SRC_DIR}/wheels
+  --manifest-path="${SRC_DIR}"/quil-cli/Cargo.toml \
+  --out "${SRC_DIR}"/wheels
 
 # Update license file
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
