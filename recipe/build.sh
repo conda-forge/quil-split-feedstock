353328090b382da9cefcd6417f08612fa3c9d59e
memento <claude.rc@gmail.com>
2024-07-04 17:20:11 -0500

    (dbg) build recipes locally with cargo and python -m build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,41 +1,23 @@
 #!/bin/bash
 
 set -ex
-# Try to lock the versions in Cargo.toml
-grep version "${SRC_DIR}"/quil-py/Cargo.toml
-grep version "${SRC_DIR}"/quil-cli/Cargo.toml
 
-if [[ $(uname) == Linux ]]; then
-  sed -i -E 's/version\s*=\s*"0\.11\.0"/version = "0.10.0"/' "${SRC_DIR}"/quil-py/Cargo.toml
-  # sed -i -E 's/version\s*=\s*"0\.27\.0"/version = "=0.26.0"/' "${SRC_DIR}"/quil-cli/Cargo.toml
-elif [[ $(uname) == Darwin ]]; then
-  sed -i '' -E 's/version\s*=\s*"0\.11\.0"/version = "0.10.0"/' "${SRC_DIR}"/quil-py/Cargo.toml
-  # sed -i '' -E 's/version\s*=\s*"0\.27\.0"/version = "=0.26.0"/' "${SRC_DIR}"/quil-cli/Cargo.toml
-else
-  echo "Unsupported OS"
-  exit 1
-fi
+cd "${SRC_DIR}"/quil-rs
+  cargo build
+cd ..
 
-# Build quil-py and quil-cli wheels
-maturin build \
-  --release \
-  --manifest-path="${SRC_DIR}"/quil-rs/Cargo.toml \
-  --strip \
-  --cargo-extra-args="+nightly -Zno-index-update" \
-  --out "${SRC_DIR}"/wheels
-maturin build \
-  --release \
-  --manifest-path="${SRC_DIR}"/quil-py/Cargo.toml \
-  --strip \
-  --cargo-extra-args="+nightly -Zno-index-update" \
-  --out "${SRC_DIR}"/wheels
-maturin build \
-  --release \
-  --manifest-path="${SRC_DIR}"/quil-cli/Cargo.toml \
-  --strip \
-  --cargo-extra-args="+nightly -Zno-index-update" \
-  --out "${SRC_DIR}"/wheels
+cd "${SRC_DIR}"/quil-py
+  python -m build --no-isolation --wheel --outdir "${SRC_DIR}"/wheels
+cd ..
+
+cd "${SRC_DIR}"/quil-cli
+  cargo build
+  maturin build \
+    --release \
+    --strip \
+    --offline \
+    --out "${SRC_DIR}"/wheels
+cd ..
 
 # Update license file
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
