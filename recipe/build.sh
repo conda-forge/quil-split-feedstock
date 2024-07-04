4047cbd0e0ddc8177b3fde1c928c2131a303f7f7
memento <claude.rc@gmail.com>
2024-07-03 19:22:26 -0500

    (dbg) trying locking the versions in Cargo.toml - take 4

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,11 @@ grep version "${SRC_DIR}"/quil-py/Cargo.toml
 grep version "${SRC_DIR}"/quil-cli/Cargo.toml
 
 if [[ $(uname) == Linux ]]; then
-  sed -i -E 's/version\s*=\s*"0.11.0/version = "0.10.0/' "${SRC_DIR}"/quil-py/Cargo.toml
-  sed -i -E 's/version\s*=\s*"0.26.0/version = "=0.26.0/' "${SRC_DIR}"/quil-cli/Cargo.toml
+  sed -i -E 's/version\s*=\s*".*"/version = "0.10.0/' "${SRC_DIR}"/quil-py/Cargo.toml
+  sed -i -E 's/version\s*=\s*".*"/version = "=0.26.0/' "${SRC_DIR}"/quil-cli/Cargo.toml
 elif [[ $(uname) == Darwin ]]; then
-  sed -i '' -E 's/version\s*=\s*"0.11.0/version = "0.10.0/' "${SRC_DIR}"/quil-py/Cargo.toml
-  sed -i '' -E 's/version\s*=\s*"0.26.0/version = "=0.26.0/' "${SRC_DIR}"/quil-cli/Cargo.toml
+  sed -i '' -E 's/version\s*=\s*".*"/version = "0.10.0"/' "${SRC_DIR}"/quil-py/Cargo.toml
+  sed -i '' -E 's/version\s*=\s*".*"/version = "=0.26.0"/' "${SRC_DIR}"/quil-cli/Cargo.toml
 else
   echo "Unsupported OS"
   exit 1
@@ -21,13 +21,11 @@ maturin build \
   --release \
   --manifest-path="${SRC_DIR}"/quil-py/Cargo.toml \
   --strip \
-  -Zno-index-update \
   --out "${SRC_DIR}"/wheels
 maturin build \
   --release \
   --manifest-path="${SRC_DIR}"/quil-cli/Cargo.toml \
   --strip \
-  -Zno-index-update \
   --out "${SRC_DIR}"/wheels
 
 # Update license file
