14f1ec429bbd0a6cf773768d781c53901e27d285
memento <claude.rc@gmail.com>
2024-07-03 19:15:23 -0500

    (dbg) trying locking the versions in Cargo.toml - take 3

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -ex
 # Try to lock the versions in Cargo.toml
+grep version "${SRC_DIR}"/quil-py/Cargo.toml
+grep version "${SRC_DIR}"/quil-cli/Cargo.toml
+
 if [[ $(uname) == Linux ]]; then
   sed -i -E 's/version\s*=\s*"0.11.0/version = "0.10.0/' "${SRC_DIR}"/quil-py/Cargo.toml
   sed -i -E 's/version\s*=\s*"0.26.0/version = "=0.26.0/' "${SRC_DIR}"/quil-cli/Cargo.toml
@@ -18,11 +21,13 @@ maturin build \
   --release \
   --manifest-path="${SRC_DIR}"/quil-py/Cargo.toml \
   --strip \
+  -Zno-index-update \
   --out "${SRC_DIR}"/wheels
 maturin build \
   --release \
   --manifest-path="${SRC_DIR}"/quil-cli/Cargo.toml \
   --strip \
+  -Zno-index-update \
   --out "${SRC_DIR}"/wheels
 
 # Update license file
