c3350ef737c079b9cdd96c5cb00164e7a5a707a2
memento <claude.rc@gmail.com>
2024-07-05 14:46:15 -0500

    MNT: v0.27.0: add quil-py/cli version verification step

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+# This attempts to verify that the version numbers for quil-py and quil-cli match with the ones we build
+# All the upstream sources are the same for a given quil-rs version, however, it occurred that only the
+# quil-rs version was updated, causing the actual quil-py distributed to be a different version than
+# advertised. This test is meant to fail on such discrepancies.
+diff -r verify-quil-py/quil-py "${SRC_DIR}"/quil-py
+diff -r verify-quil-cli/quil-cli "${SRC_DIR}"/quil-cli
+
 cd "${SRC_DIR}"/quil-rs
   cargo build
 cd ..
