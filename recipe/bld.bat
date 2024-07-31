bcd6aa6709b3c2e2142b72c789c1ea89e4aeea81
memento <claude.rc@gmail.com>
2024-07-31 15:32:55 -0500

    (ref) re-arch build process to handel independent updates

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,8 @@
 @echo off
 REM Build
-call maturin build --release --manifest-path=%SRC_DIR%\quil-py\Cargo.toml --out %SRC_DIR%\wheels
-call maturin build --release --manifest-path=%SRC_DIR%\quil-cli\Cargo.toml --out %SRC_DIR%\wheels
+call maturin build --release --manifest-path=%SRC_DIR%\quil-py\quil-py\Cargo.toml --out %SRC_DIR%\wheels
+call maturin build --release --manifest-path=%SRC_DIR%\quil-cli\quil-cli\Cargo.toml --out %SRC_DIR%\wheels
 
-call cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+pushd %SRC_DIR%\quil-py
+    call cargo-bundle-licenses --format yaml --output %RECIPE_DIR%\THIRDPARTY.yml
+popd
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,26 +2,19 @@
 
 set -ex
 
-# This attempts to verify that the version numbers for quil-py and quil-cli match with the ones we build
-# All the upstream sources are the same for a given quil-rs version, however, it occurred that only the
-# quil-rs version was updated, causing the actual quil-py distributed to be a different version than
-# advertised. This test is meant to fail on such discrepancies.
-diff -r verify-quil-py/quil-py "${SRC_DIR}"/quil-py
-diff -r verify-quil-cli/quil-cli "${SRC_DIR}"/quil-cli
-
-cd "${SRC_DIR}"/quil-py
+pushd "${SRC_DIR}"/quil-py/quil-py
   maturin build \
     --release \
     --strip \
     --out "${SRC_DIR}"/wheels
-cd ..
 
-cd "${SRC_DIR}"/quil-cli
+  cargo-bundle-licenses --format yaml --output "${RECIPE_DIR}"/THIRDPARTY.yml
+popd
+
+pushd "${SRC_DIR}"/quil-cli/quil-cli
   maturin build \
     --release \
     --strip \
     --out "${SRC_DIR}"/wheels
-cd ..
+popd
 
-# Update license file
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
